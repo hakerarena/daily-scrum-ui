21989ef556433aae08bf22f83d5fe4bc08a0a580
Sachin Sarkar <sachin.sarkar98@gmail.com>
2025-03-11 10:26:03 -1200

    feat: :sparkles: Different shades of day

--- src/app/feature-components/navbar/navbar.component.ts
+++ src/app/feature-components/navbar/navbar.component.ts
@@ -5,38 +5,69 @@ import { SidebarService } from '../../services/sidebar.service';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
-  standalone: false
+  standalone: false,
 })
 export class NavbarComponent implements OnInit {
   username: string = '';
   isSidebarOpen = false;
-  currentDate: Date = new Date();
-  currentTime: string = '';
+  currentDate = new Date();
+  timeClass: string = 'sunrise';
+  timeIconClass: string = 'sun';
 
   constructor(private sidebarService: SidebarService) {
     this.username = localStorage.getItem('email') || 'User';
-    this.sidebarService.sidebarState.subscribe(isSidebarOpen => this.isSidebarOpen = isSidebarOpen);
+    this.sidebarService.sidebarState.subscribe(
+      (isSidebarOpen) => (this.isSidebarOpen = isSidebarOpen)
+    );
   }
 
   ngOnInit(): void {
     this.updateTime();
-    setInterval(() => {
-      this.updateTime();
-    }, 1000);
+    setInterval(() => this.updateTime(), 1000);
   }
 
   updateTime() {
-    const now = new Date();
-    this.currentDate = now;
-    this.currentTime = now.toLocaleTimeString();
+    this.currentDate = new Date();
+    const hour = this.currentDate.getHours();
+    const minute = this.currentDate.getMinutes();
+    const totalMinutes = hour * 60 + minute;
+
+    if (totalMinutes >= 5 * 60 && totalMinutes < 12 * 60) {
+      this.timeClass = 'sunrise';
+      this.timeIconClass = 'sun';
+    } else if (totalMinutes >= 12 * 60 && totalMinutes < 18 * 60) {
+      this.timeClass = 'sunset';
+      this.timeIconClass = 'sun';
+    } else {
+      this.timeClass = 'moonlight';
+      this.timeIconClass = 'moon';
+    }
+  }
+
+  getIconTransform() {
+    const hour = this.currentDate.getHours();
+    const minute = this.currentDate.getMinutes();
+    const totalMinutes = hour * 60 + minute;
+    let rotation = 0;
+
+    if (totalMinutes >= 5 * 60 && totalMinutes < 18 * 60) {
+      // Sunrise to Sunset
+      rotation = ((totalMinutes - 5 * 60) / (13 * 60)) * 180;
+    } else {
+      // Sunset to Sunrise
+      if (totalMinutes >= 18 * 60) {
+        rotation = ((totalMinutes - 18 * 60) / (11 * 60)) * 180 + 180;
+      } else {
+        rotation = ((totalMinutes + 6 * 60) / (11 * 60)) * 180 + 180;
+      }
+    }
+    return `rotate(${rotation}deg)`;
   }
 
   openUpdateStatus() {
-    this.isSidebarOpen ? this.sidebarService.closeSidebar() : this.sidebarService.openSidebar();
-  }
-
-  closeSidebar() {
-    this.isSidebarOpen = false;
+    this.isSidebarOpen
+      ? this.sidebarService.closeSidebar()
+      : this.sidebarService.openSidebar();
   }
 
   logout() {
